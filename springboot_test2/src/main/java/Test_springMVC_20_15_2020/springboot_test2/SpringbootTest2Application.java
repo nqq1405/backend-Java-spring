7340d509ce35f20f81054f15889e91237c124478
Nguyen Quy Quang <quang20hn@gmail.com>
2020-12-16 09:44:33 +0700

    MainApp

---
+++ springboot_test2/src/main/java/Test_springMVC_20_15_2020/springboot_test2/SpringbootTest2Application.java
@@ -0,0 +1,15 @@
+package Test_springMVC_20_15_2020.springboot_test2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTest2Application {
+
+	public static void main(String[] args) {
+		System.err.println("begin");
+		SpringApplication.run(SpringbootTest2Application.class, args);
+		System.err.println("end");
+	}
+
+}
